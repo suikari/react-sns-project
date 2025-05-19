81ea59a1998e9b649b184c9409b29e4b408988b2
suikari <suikari@naver.com>
2025-05-19 12:57:17 +0900

    알림 백엔드 무한스크롤방식으로 수정 (프론트쪽은 10개로 고정 차후수정 예정)

--- server/controllers/notificationsController.js
+++ server/controllers/notificationsController.js
@@ -3,13 +3,21 @@ require('dotenv').config();
 
 // 현재 로그인 유저의 알림 조회
 exports.getNotifications = async (req, res) => {
-    const userId = req.user.id;
+  const userId = req.user.id;
+  const limit = parseInt(req.query.limit) || 10; // 기본값 10
+  const offset = parseInt(req.query.offset) || 0; // 기본값 0
+
+  try {
     const [rows] = await db.query(
-      `SELECT * FROM tbl_notifications WHERE userId = ? ORDER BY createdAt DESC`,
-      [userId]
+      `SELECT * FROM tbl_notifications WHERE userId = ? ORDER BY createdAt DESC LIMIT ? OFFSET ?`,
+      [userId, limit, offset]
     );
     res.json(rows);
-  };
+  } catch (error) {
+    console.error("Error fetching notifications:", error);
+    res.status(500).json({ message: "Failed to fetch notifications." });
+  }
+};
   
   // 알림 읽음 처리
   exports.setReadNotifications  = async (req, res) => {
